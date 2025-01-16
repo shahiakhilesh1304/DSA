cf84492446b311810ad09432a0ac1642dee98193
shahiakhilesh1304 <akhilesh.shahi13@gmail.com>
2025-01-17 01:05:45 +0530

    Raw Push to check the branch flow

--- Ruler/util.py
+++ Ruler/util.py
@@ -2,13 +2,13 @@ from Ruler.Analyzer import ComplexityAnalyzer
 import inspect
 
 
-def start(func_name):
+def start(function):
     analyzer = ComplexityAnalyzer()
-    source = inspect.getsource(func_name)
+    source = inspect.getsource(function)
     return analyzer,source
 
-def report(analyzer,source,func_name):
-    source = inspect.getsource(func_name)
+def report(analyzer,source,function):
+    source = inspect.getsource(function)
     result = analyzer.analyze_source(source)
     print(f"\n\n\n\nComplexity Report \n\nFunction: {result['name']}")
     print(f"Complexity: {result['complexity']}")
