67d20a476a42d555afac2f59af0a07356ce2b07a
Akhilesh Pratap Shahi <akhilesh.shahi13@gmail.com>
2025-02-16 00:48:35 +0530

    Revert "Streak maintainer"

--- Solution/Question37/threeSum.py
+++ Solution/Question37/threeSum.py
@@ -6,24 +6,35 @@ def threeSum(nums: list[int]) -> list[list[int]]:
         nums.sort() 
         result = []
 
-        for i in range(len(nums) - 2):     
+        for i in range(len(nums) - 2):
+            
             if i > 0 and nums[i] == nums[i - 1]:
-                continue    
+                continue
+
+          
             left, right = i + 1, len(nums) - 1
+
             while left < right:
                 total = nums[i] + nums[left] + nums[right]
+                
                 if total < 0:
                     left += 1 
                 elif total > 0:
                     right -= 1 
-                else:   
+                else:
+                   
                     result.append([nums[i], nums[left], nums[right]])
+
+                   
                     while left < right and nums[left] == nums[left + 1]:
                         left += 1
                     while left < right and nums[right] == nums[right - 1]:
                         right -= 1
+                    
+                  
                     left += 1
                     right -= 1
+
         return result
     
 if __name__ == "__main__":
